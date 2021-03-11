125e5078bf349de2a23852108ddffb2fe800a9e9
Gerardo Fernández <gefe.86@gmail.com>
2021-03-11 12:39:37 +0100

    refactor

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -15,70 +15,35 @@ var GildedRose = function () {
   GildedRose.updateQuality(items);
 };
 
+const THRESHOLD_ONE_FOR_SELL_IN = 10;
+const THRESHOLD_TWO_FOR_SELL_IN = 5;
+
 GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      //TODO: Improve this code.
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
+    const item = items[i];
+
+    item.decrementSellIn();
+
+    if (item.isBackstagePass()) {
+      if (item.isAfterConcertDay()) {
+        item.setQuality(0);
+      } else {
+        item.incrementQuality();
+        if (item.sellIn <= THRESHOLD_TWO_FOR_SELL_IN) {
+          item.incrementQuality();
+        }
+        if (item.sellIn <= THRESHOLD_ONE_FOR_SELL_IN) {
+          item.incrementQuality();
         }
       }
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 6) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            // See revision number 2394 on SVN.
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
+      if (item.isAfterConcertDay()) {
+        item.decrementQuality();
+        item.decrementQuality();
+      } else {
+        item.decrementQuality();
       }
     }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1;
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          //TODO: Fix this.
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-            items[i].quality = 0;
-      } // of for.
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-      if (items[i].quality > 50) items[i].quality = 50;
   }
   return items;
 };
--- src/Item.js
+++ src/Item.js
@@ -1,5 +1,54 @@
-var Item = function (name, sellIn, quality) {
+const MAX_QUALITY = 50;
+
+var Item = function(name, sellIn, quality) {
   this.name = name;
   this.sellIn = sellIn;
   this.quality = quality;
+
+  this.setQuality = function(quality) {
+    if (this.isLegendary()) {
+      return;
+    }
+    if (quality > MAX_QUALITY) {
+      this.quality = MAX_QUALITY;
+      return;
+    }
+    this.quality = quality;
+  }
+
+  this.incrementQuality = function() {
+    if (this.isLegendary()) {
+      return;
+    }
+    this.quality = this.quality + 1;
+    if (this.quality > MAX_QUALITY) {
+      this.quality = MAX_QUALITY;
+    }
+  }
+
+  this.decrementQuality = function() {
+    if (this.isLegendary()) {
+      return;
+    }
+    this.quality = this.quality - 1;
+  }
+
+  this.decrementSellIn = function() {
+    if (this.isLegendary()) {
+      return;
+    }
+    this.sellIn = this.sellIn - 1;
+  }
+
+  this.isBackstagePass = function() {
+    return ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"].includes(this.name);
+  }
+
+  this.isLegendary = function() {
+    return "Sulfuras, Hand of Ragnaros" === this.name;
+  }
+
+  this.isAfterConcertDay = function() {
+    return this.sellIn < 0;
+  }
 };
